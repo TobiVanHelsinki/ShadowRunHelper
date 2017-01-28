cac195da7fadb75dbdecd6bc3301bbf9c904fcff
Tobias Pabst <TobivanHelsinki@live.de>
2017-01-28 21:57:46 +0100

    more refactoring

--- ShadowRunHelper/ShadowRunHelper/Ressourcen/TypNamen.cs
+++ ShadowRunHelper/ShadowRunHelper/Ressourcen/TypNamen.cs
@@ -32,30 +32,5 @@ namespace ShadowRunHelper
         Panzerung = 17,
         Eigenschaft = 18,
     }
-
-    public static class TypNamen
-    {
-
-        public static string GetName_Controller(string input)
-        {
-            Dictionary<String, String> Ressources = new Dictionary<String, String>();
-            Ressources.Add("ShadowRunHelper.CharController.Fertigkeit", "Fertigkeit");
-            Ressources.Add("ShadowRunHelper.CharController.Handlung", "Handlung");
-            Ressources.Add("ShadowRunHelper.CharController.Attribut", "Attribut");
-            Ressources.Add("ShadowRunHelper.CharController.Item", "Item");
-            Ressources.Add("ShadowRunHelper.CharController.Programm", "Programm");
-            Ressources.Add("ShadowRunHelper.CharController.Vorteil", "Vorteil");
-            Ressources.Add("ShadowRunHelper.CharController.Nachteil", "Nachteil");
-            Ressources.Add("ShadowRunHelper.CharController.Panzerung", "Panzerung");
-            Ressources.Add("ShadowRunHelper.CharController.CyberDeck", "CyberDeck");
-            Ressources.Add("ShadowRunHelper.CharController.Kommlink", "Kommlink");
-            Ressources.Add("ShadowRunHelper.CharController.Vehikel", "Vehikel");
-            Ressources.Add("ShadowRunHelper.CharController.Nahkampfwaffe", "Nahkampfwaffe");
-            Ressources.Add("ShadowRunHelper.CharController.Fernkampfwaffe", "Fernkampfwaffe");
-
-            Ressources.TryGetValue(input, out input);
-
-            return input;
-        }
-    }
+    
 }
