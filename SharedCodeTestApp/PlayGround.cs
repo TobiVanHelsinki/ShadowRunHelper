6709a4d0b774fe81d6738c30e57e4270efebf62a
Tobias Pabst <TobivanHelsinki@live.de>
2018-03-22 10:30:23 +0100

    added 2 more projects: experiments and a uwp unit test app

---
+++ SharedCodeTestApp/PlayGround.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using ShadowRunHelper;
+using ShadowRunHelper.CharModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCodeTestApp
+{
+    [TestClass]
+    public class PlayGround
+    {
+
+    }
+}
