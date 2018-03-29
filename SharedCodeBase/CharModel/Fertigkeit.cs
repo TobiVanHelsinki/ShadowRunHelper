bd0a8229d9a12069da0cecb1b582d26bb07c9fc1
Tobias Pabst <TobivanHelsinki@live.de>
2018-03-29 10:01:28 +0200

    removed old code

--- SharedCodeBase/CharModel/Fertigkeit.cs
+++ SharedCodeBase/CharModel/Fertigkeit.cs
@@ -1,28 +1,9 @@
-﻿using ShadowRunHelper.Model;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ShadowRunHelper.CharModel
 {
     public class Fertigkeit : Thing
     {
-        //[Used_List]
-        //public ObservableThingListEntryCollection PoolZusammensetzung { get; set; }
-
-        //private double _Pool = 0;
-        //[Used_UserAttribute]
-        //public double Pool
-        //{
-        //    get { return _Pool; }
-        //    set
-        //    {
-        //        if (value != _Pool)
-        //        {
-        //            _Pool = value;
-        //            NotifyPropertyChanged();
-        //        }
-        //    }
-        //}
-        
         public static IEnumerable<ThingDefs> Filter = new List<ThingDefs>()
             {
                 ThingDefs.Handlung, ThingDefs.Connection, ThingDefs.Fertigkeit
@@ -30,17 +11,7 @@ namespace ShadowRunHelper.CharModel
 
         public Fertigkeit() : base()
         {
-            //PoolZusammensetzung = new ObservableThingListEntryCollection();
             LinkedThings.SetFilter(Filter);
-            //PoolZusammensetzung.OnCollectionChangedCall(() =>
-            //{ Pool = Wert + PoolZusammensetzung.Recalculate(); });
-
-            //PropertyChanged += (s, e)=> { if (e.PropertyName == "Wert") Pool = Wert + PoolZusammensetzung.Recalculate(); };
         }
-
-        //public override string ToString()
-        //{
-        //    return typ + (typ != "" ? ": " : "") + bezeichner + " " + WertCalced + (Zusatz != "" ? "+" : "") + Zusatz;
-        //}
     }
 }
