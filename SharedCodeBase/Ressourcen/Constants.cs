48c3dd4b6885662ae90116e775764ece3956fe7e
Tobi van Helsinki <TobivanHelsinki@live.de>
2018-04-21 12:19:04 +0200

    std removings

--- SharedCodeBase/Ressourcen/Constants.cs
+++ SharedCodeBase/Ressourcen/Constants.cs
@@ -95,9 +95,8 @@ namespace ShadowRunHelper
         #endregion
         #region IAPs
         public const string IAP_FEATUREID_ADFREE_365 = "IAP_ADFREE_365";
-        public const string IAP_FEATUREID_ADFREE = "IAP_ADFREE";
+        public const string IAP_FEATUREID_ADFREE = "9PG66W9790JB";
         public const string IAP_FEATUREID_TEE = "9N6G5Z236BTH";
-        //public const string IAP_FEATUREID_TEE = "TEE";
         public static bool IAP_HIDEADS { get; internal set; }
 
         #endregion
