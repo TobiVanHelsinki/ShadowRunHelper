cf2ea014d0d52bc1e289ddf7f62784ce7f643288
TobiVanHelsinki <TobivanHelsinki@live.de>
2018-05-12 11:45:17 +0200

    implemented debug char

--- ShadowRunHelper.Win/UI/Pages/MainPage.xaml.cs
+++ ShadowRunHelper.Win/UI/Pages/MainPage.xaml.cs
@@ -351,5 +351,26 @@ namespace ShadowRunHelper.UI
 
         void EditBox_PreviewKeyDown(object sender, KeyRoutedEventArgs e) => SharePageFunctions.EditBox_UpDownKeys(sender, e);
 
+        private void CreateDebugChar(object sender, RoutedEventArgs e)
+        {
+            Model.MainObject = new CharHolder();
+            foreach (var item in TypeHelper.ThingTypeProperties.Where(x=>x.Usable))
+            {
+                CharModel.Thing NewThing;
+                try
+                {
+                     NewThing = Model.MainObject.Add(item.ThingType);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                NewThing.Bezeichner = "TestName";
+                NewThing.Notiz = "Dies ist eine Notiz\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+                NewThing.Wert = 5;
+                NewThing.Zusatz = "+ xW55";
+                NewThing.Typ = "Typ";
+            }
+        }
     }
 }
