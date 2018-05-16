50acd7e7c906f144d6243913e7db9de6251a58b6
TobiVanHelsinki <TobivanHelsinki@live.de>
2018-05-16 13:34:16 +0200

    added filename to apppacket

--- ShadowRunHelper.Win/UI/ClassicDialog.xaml.cs
+++ ShadowRunHelper.Win/UI/ClassicDialog.xaml.cs
@@ -18,12 +18,12 @@ namespace ShadowRunHelper.Win.UI
         private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
             var set = SettingsModel.I.ExportAllSettings();
-            var files = new List<string>();
+            var files = new List<(string, string)>();
             var List = await CharHolderIO.CurrentIO.GetListofFiles(new FileInfoClass() { Fileplace = CharHolderIO.GetCurrentSavePlace(), Filepath = CharHolderIO.GetCurrentSavePath(), FolderToken = Constants.ACCESSTOKEN_FOLDERMODE }, UserDecision.ThrowError, Constants.LST_FILETYPES_CHAR);
             foreach (var item in List)
             {
                 var content = await CharHolderIO.CurrentIO.LoadFileContent(item, null, UserDecision.ThrowError);
-                files.Add(content.strFileContent);
+                files.Add((content.Info.Filename, content.strFileContent));
             }
             string ser = JsonConvert.SerializeObject((set, files));
             await CharHolderIO.CurrentIO.SaveFileContent(ser, new FileInfoClass(Place.Extern, "ShadowRunHelperAppContent.SRHApp1","" ), UserDecision.AskUser);
