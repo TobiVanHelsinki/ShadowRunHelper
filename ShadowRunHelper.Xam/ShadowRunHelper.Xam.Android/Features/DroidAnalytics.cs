8c406d169f3d12200d7bda70df1ef24eafe5920c
tobiv <tobivanhelsinki@live.de>
2018-09-24 16:04:03 +0200

    updatet nuget and implemented appcenter in droid

--- ShadowRunHelper.Xam/ShadowRunHelper.Xam.Android/Features/DroidAnalytics.cs
+++ ShadowRunHelper.Xam/ShadowRunHelper.Xam.Android/Features/DroidAnalytics.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using System.Collections.Generic;
 
 namespace ShadowRunHelper
@@ -7,10 +9,12 @@ namespace ShadowRunHelper
     {
         public void Init()
         {
+            AppCenter.Start(Constants.AppCenterID, typeof(Crashes), typeof(Analytics)); // zu lange, nach mainwindow creation
         }
 
         public void TrackEvent(string name, IDictionary<string, string> properties = null)
         {
+            Microsoft.AppCenter.Analytics.Analytics.TrackEvent(name, properties);
         }
     }
 }
