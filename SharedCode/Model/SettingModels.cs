89292d08f067338f212ccc4aef538ec8ab26669b
Tobias Pabst <TobivanHelsinki@live.de>
2018-11-13 09:49:50 +0100

    adjusted for new TLIB Settings Model

--- SharedCode/Model/SettingModels.cs
+++ SharedCode/Model/SettingModels.cs
@@ -12,163 +12,50 @@ namespace ShadowRunHelper
     public class SettingsModel : SharedSettingsModel
     {
         #region Settings
-        [RoamingSettingAttribute]
-        public bool DISABLE_TIPS
-        {
-            get => PlatformSettings.GetBoolRoaming(Constants.CONTAINER_SETTINGS_DISABLE_TIPS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_DISABLE_TIPS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
 
-        [LocalSettingAttribute]
-        public bool IAP_HIDEADS
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_IAP_HIDEADS);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_IAP_HIDEADS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-       
-        [LocalSettingAttribute]
-        public string LAST_APP_VERSION
-        {
-            get => PlatformSettings.GetStringLocal(Constants.CONTAINER_SETTINGS_LAST_APP_VERSION);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_APP_VERSION, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_DISABLE_TIPS", false, SaveType.Roaming)]
+        public bool DISABLE_TIPS { get => Get(); set => Set(value); }
 
-        [LocalSettingAttribute]
-        public ProjectPages LAST_PAGE
-        {
-            get => (ProjectPages)PlatformSettings.GetIntLocal(Constants.CONTAINER_SETTINGS_LAST_PAGE);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_PAGE, (int)value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_IAP_HIDEADS", false, SaveType.Local)]
+        public bool IAP_HIDEADS { get => Get(); set => Set(value); }
 
-        [LocalSettingAttribute]
-        public bool FORCE_LOAD_CHAR_ON_START
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_FORCE_LOAD_CHAR_ON_START);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_FORCE_LOAD_CHAR_ON_START, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_LAST_APP_VERSION", "", SaveType.Local)]
+        public string LAST_APP_VERSION { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public bool AUTO_SAVE
-        {
-            get => PlatformSettings.GetBoolRoaming(Constants.CONTAINER_SETTINGS_AUTO_SAVE);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_AUTO_SAVE, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_LAST_PAGE", ProjectPages.Char, SaveType.Local, typeof(int))]
+        public ProjectPages LAST_PAGE { get => (ProjectPages)Get(); set => Set(value); }
 
-        [LocalSettingAttribute]
-        public bool IAP_PREMIUM_BADGE
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_IAP_PREMIUM_BADGE);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_IAP_PREMIUM_BADGE, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_FORCE_LOAD_CHAR_ON_START", false, SaveType.Local)]
+        public bool FORCE_LOAD_CHAR_ON_START { get => Get(); set => Set(value); }
 
-        [LocalSettingAttribute]
-        public bool TUT_SHOWN_1
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_TUT_SHOWN_1);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_TUT_SHOWN_1, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_AUTO_SAVE", false, SaveType.Roaming)]
+        public bool AUTO_SAVE { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int START_COUNT_DB
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_START_COUNT_DB);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_START_COUNT_DB, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("IAP_BADGE", false, SaveType.Local)]
+        public bool IAP_PREMIUM_BADGE { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int START_COUNT
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_START_COUNT);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_START_COUNT, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_TUT_SHOWN_1", false, SaveType.Local)]
+        public bool TUT_SHOWN_1 { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int COUNT_LOADINGS
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_COUNT_LOADINGS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_COUNT_LOADINGS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-        [RoamingSettingAttribute]
-        public int COUNT_SAVINGS
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_COUNT_SAVINGS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_COUNT_SAVINGS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_START_COUNT_DB", 0, SaveType.Roaming)]
+        public int START_COUNT_DB { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int COUNT_DELETIONS
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_COUNT_DELETIONS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_COUNT_DELETIONS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_START_COUNT", 0, SaveType.Roaming)]
+        public int START_COUNT { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int COUNT_CREATIONS
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_COUNT_CREATIONS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_COUNT_CREATIONS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
+        [Setting("SETTINGS_COUNT_LOADINGS", 0, SaveType.Roaming)]
+        public int COUNT_LOADINGS { get => Get(); set => Set(value); }
+        [Setting("SETTINGS_COUNT_SAVINGS", 0, SaveType.Roaming)]
+        public int COUNT_SAVINGS { get => Get(); set => Set(value); }
 
-        [RoamingSettingAttribute]
-        public int AUTO_SAVE_INTERVAL_MS
-        {
-            get => PlatformSettings.GetIntRoaming(Constants.CONTAINER_SETTINGS_AUTO_SAVE_INTERVAL_MS);
+        [Setting("SETTINGS_COUNT_DELETIONS", 0, SaveType.Roaming)]
+        public int COUNT_DELETIONS { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_COUNT_CREATIONS", 0, SaveType.Roaming)]
+        public int COUNT_CREATIONS { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_AUTO_SAVE_INTERVAL", 0, SaveType.Roaming)]
+        public int AUTO_SAVE_INTERVAL_MS { get {  return Get(); }
             set
             {
                 if (value > 100000)
@@ -180,109 +67,27 @@ namespace ShadowRunHelper
                     value = 1000;
                 }
                 System.Diagnostics.Debug.WriteLine("value: " + value);
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_AUTO_SAVE_INTERVAL_MS, value);
-                Instance.NotifyPropertyChanged();
+                Set(value);
             }
         }
+
+        [Setting("FIRST_START", true, SaveType.Local)]
+        public bool FIRST_START { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_LOAD_CHAR_ON_START", false, SaveType.Local)]
+        public bool LOAD_CHAR_ON_START { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_START_AFTER_EDIT", true, SaveType.Roaming)]
+        public bool START_AFTER_EDIT { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_FILENAME_USEPROGRESS", false, SaveType.Roaming)]
+        public bool FILENAME_USEPROGRESS { get => Get(); set => Set(value); }
+
+        [Setting("SETTINGS_FILENAME_USEDATE", false, SaveType.Roaming)]
+        public bool FILENAME_USEDATE { get => Get(); set => Set(value); }
         
-
-        [LocalSettingAttribute]
-        public bool FIRST_START
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_FIRST_START, true);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_FIRST_START, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-        [LocalSettingAttribute]
-        public bool LOAD_CHAR_ON_START
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_LOAD_CHAR_ON_START);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LOAD_CHAR_ON_START, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-
-        [RoamingSettingAttribute]
-        public bool START_AFTER_EDIT
-        {
-            get => PlatformSettings.GetBoolRoaming(Constants.CONTAINER_SETTINGS_START_AFTER_EDIT);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_START_AFTER_EDIT, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-
-        [LocalSettingAttribute]
-        public FileInfoClass LAST_SAVE_INFO
-        {
-            get
-            {
-                return new FileInfoClass()
-                {
-                    Filename = PlatformSettings.GetStringLocal(Constants.CONTAINER_SETTINGS_LAST_CHAR_NAME),
-                    Filepath = PlatformSettings.GetStringLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PATH),
-                    Fileplace = (Place)PlatformSettings.GetIntLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PLACE),
-                    Token = PlatformSettings.GetStringLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_TOKEN)
-                };
-            }
-            set
-            {
-                if (value == null)
-                {
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_CHAR_NAME, Constants.CONTAINER_SETTINGS_LAST_CHAR_NAME_STD);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PATH, Constants.CONTAINER_SETTINGS_LAST_SAVE_PATH_STD);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PLACE, Constants.CONTAINER_SETTINGS_LAST_SAVE_PLACE_STD);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_TOKEN, Constants.CONTAINER_SETTINGS_LAST_SAVE_PLACE_STD);
-                }
-                else
-                {
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_CHAR_NAME, value.Filename);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PATH, value.Filepath);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_PLACE, (int)value.Fileplace);
-                    PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_LAST_SAVE_TOKEN, value.Token);
-                }
-                Instance.NotifyPropertyChanged();
-            }
-        }
-
-        [RoamingSettingAttribute]
-        public bool FILENAME_USEPROGRESS
-        {
-            get => PlatformSettings.GetBoolRoaming(Constants.CONTAINER_SETTINGS_FILENAME_USEPROGRESS);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_FILENAME_USEPROGRESS, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-
-        [RoamingSettingAttribute]
-        public bool FILENAME_USEDATE
-        {
-            get => PlatformSettings.GetBoolRoaming(Constants.CONTAINER_SETTINGS_FILENAME_USEDATE);
-            set
-            {
-                PlatformSettings.SetRoaming(Constants.CONTAINER_SETTINGS_FILENAME_USEDATE, value);
-                Instance.NotifyPropertyChanged();
-            }
-        }
-
-        [LocalSettingAttribute]
-        public bool CHARINTEMPSTORE
-        {
-            get => PlatformSettings.GetBoolLocal(Constants.CONTAINER_SETTINGS_CHARINTEMPSTORE);
-            set
-            {
-                PlatformSettings.SetLocal(Constants.CONTAINER_SETTINGS_CHARINTEMPSTORE, value);
-                NotifyPropertyChanged();
-            }
-        }
+        [Setting("CHARINTEMPSTORE", false, SaveType.Local)]
+        public bool CHARINTEMPSTORE { get => Get(); set => Set(value); }
 
         #endregion Settings
 
@@ -294,7 +99,7 @@ namespace ShadowRunHelper
                 SharedIO.CurrentIO.CreateSaveContainer();
                 instance = new SettingsModel
                 {
-                    UsedConstants = typeof(Constants)
+                    //UsedConstants = typeof(Constants)
                 };
             }
             instance.PropertyChanged += SettingsChanged;
@@ -388,4 +193,5 @@ namespace ShadowRunHelper
         #endregion
 
     }
+
 }
