4107debf4a7494d019e7be766b188bfb84a61669
Tobias Pabst <TobivanHelsinki@live.de>
2018-11-13 09:50:28 +0100

    fix: old code comment

--- Experiments/MainPage.xaml.cs
+++ Experiments/MainPage.xaml.cs
@@ -13,8 +13,8 @@ namespace Experiments
             //TestBlock.Text = Lib_STD.Class1.MyMethod();
             TLIB_UWP.Init.Do();
             TAPPLICATION_UWP.Init.Do();
-            TestBlock.Text = TLIB.PlatformHelper.StringHelper.GetPrefix(TLIB.PlatformHelper.PrefixType.AppUserData);
-            TestBlock2.Text = TAPPLICATION.Model.SharedSettingsModel.PlatformSettings.GetIntLocal("d", 77).ToString();
+            //TestBlock.Text = TLIB.PlatformHelper.StringHelper.GetPrefix(TLIB.PlatformHelper.PrefixType.AppUserData);
+            //TestBlock2.Text = TAPPLICATION.Model.SharedSettingsModel.PlatformSettings.GetIntLocal("d", 77).ToString();
         }
     }
 }
