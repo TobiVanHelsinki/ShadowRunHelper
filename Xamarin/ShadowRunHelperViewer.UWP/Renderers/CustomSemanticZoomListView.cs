f1c1e60882df54382a1b92ea11f7e61498b2984b
Tobi van Helsinki <TobivanHelsinki@live.de>
2019-10-21 16:44:28 +0200

    bug fixes and try for a custom template

--- Xamarin/ShadowRunHelperViewer.UWP/Renderers/CustomSemanticZoomListView.cs
+++ Xamarin/ShadowRunHelperViewer.UWP/Renderers/CustomSemanticZoomListView.cs
@@ -1,6 +1,8 @@
 ﻿///Author: Tobi van Helsinki
 
 using ShadowRunHelperViewer.UWP.Renderers;
+using System.IO;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
 using wincontrol = Windows.UI.Xaml.Controls;
@@ -23,8 +25,33 @@ namespace ShadowRunHelperViewer.UWP.Renderers
             if (Control is wincontrol.SemanticZoom zoom)
             {
                 var defaultview = zoom.ZoomedOutView as wincontrol.GridView;
-                zoom.ZoomedOutView = new wincontrol.ListView() { ItemsSource = defaultview.ItemsSource };
+                //var count = (defaultview.ItemsSource as System.Collections.Generic.IEnumerable<object>)?.Count().ToString() ?? "";
+                //var name = "Key";
+                zoom.ZoomedOutView = new wincontrol.ListView() { ItemsSource = defaultview.ItemsSource, /*ItemTemplate = Create(name, count)*/ };
             }
+        }
+
+        /// <summary>
+        /// Creates the custom templte for the zoomed out view
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="count">The count.</param>
+        /// <returns></returns>
+        public static Windows.UI.Xaml.DataTemplate Create(string key, string count)
+        {
+            var stringReader =
+@"<DataTemplate
+    xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">
+    <StackPanel Orientation=""Horizontal"">
+        <TextBlock Text=""{Binding  }""/>
+        <TextBlock Margin=""4,0,0,0"">
+            <Run Text=""("" Margin=""0""/>
+            <Run Text=""" + Count + @""" Margin=""0""/>
+            <Run Text="")"" Margin=""0""/>
+        </TextBlock>
+    </StackPanel>
+</DataTemplate>";
+            return Windows.UI.Xaml.Markup.XamlReader.Load(stringReader) as Windows.UI.Xaml.DataTemplate;
         }
     }
 }
