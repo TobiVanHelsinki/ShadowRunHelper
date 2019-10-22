e579a8ebf89273828d045b2821cc52416beae49e
Tobi van Helsinki <TobivanHelsinki@live.de>
2019-10-22 15:47:50 +0200

    ui fixes

--- SharedCode/CharModel/CharProperty.cs
+++ SharedCode/CharModel/CharProperty.cs
@@ -11,7 +11,7 @@ using TLIB;
 
 namespace ShadowRunHelper.CharModel
 {
-    public class CharCalcProperty : INotifyPropertyChanged/*, IConvertible*/
+    public class CharCalcProperty : INotifyPropertyChanged
     {
         #region NotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
@@ -25,13 +25,6 @@ namespace ShadowRunHelper.CharModel
         #region Implicit Converter
 
         public static implicit operator CharCalcProperty(double d) => new CharCalcProperty("implicit from " + d, new Thing()) { BaseValue = d };
-
-        public static implicit operator CharCalcProperty(int d) => new CharCalcProperty("implicit from " + d, new Thing()) { BaseValue = d };
-
-        //public static explicit operator double(CharCalcProperty c)
-        //{
-        //    return c.Value;
-        //}
 
         #endregion Implicit Converter
 
@@ -206,93 +199,5 @@ namespace ShadowRunHelper.CharModel
             return target;
         }
         #endregion Deletion Handling
-
-        #region IConvertible
-
-        public TypeCode GetTypeCode()
-        {
-            return Value.GetTypeCode();
-        }
-
-        public bool ToBoolean(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToBoolean(provider);
-        }
-
-        public byte ToByte(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToByte(provider);
-        }
-
-        public char ToChar(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToChar(provider);
-        }
-
-        public DateTime ToDateTime(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToDateTime(provider);
-        }
-
-        public decimal ToDecimal(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToDecimal(provider);
-        }
-
-        public double ToDouble(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToDouble(provider);
-        }
-
-        public short ToInt16(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToInt16(provider);
-        }
-
-        public int ToInt32(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToInt32(provider);
-        }
-
-        public long ToInt64(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToInt64(provider);
-        }
-
-        public sbyte ToSByte(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToSByte(provider);
-        }
-
-        public float ToSingle(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToSingle(provider);
-        }
-
-        public string ToString(IFormatProvider provider)
-        {
-            return Value.ToString(provider);
-        }
-
-        public object ToType(Type conversionType, IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToType(conversionType, provider);
-        }
-
-        public ushort ToUInt16(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToUInt16(provider);
-        }
-
-        public uint ToUInt32(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToUInt32(provider);
-        }
-
-        public ulong ToUInt64(IFormatProvider provider)
-        {
-            return ((IConvertible)Value).ToUInt64(provider);
-        }
-        #endregion IConvertible
     }
 }
