8eab9360a60b9cbbad9b1d506e0d30bfe2af8e6e
TobiVanHelsinki <TobiVanHelsinki@live.de>
2020-04-03 19:17:00 +0200

    explore and set new android build and deploy options

--- Xamarin/ShadowRunHelperViewer.Android/Properties/AssemblyInfo.cs
+++ Xamarin/ShadowRunHelperViewer.Android/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
-﻿using System.Reflection;
+﻿//Author: Tobi van Helsinki
+
+using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("ShadowRunHelperViewer.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -19,14 +19,10 @@ using Android.App;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
@@ -34,3 +30,8 @@ using Android.App;
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
 [assembly: NeutralResourcesLanguage("en")]
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
