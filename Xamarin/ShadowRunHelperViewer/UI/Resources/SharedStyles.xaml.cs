7e88b5be3a6290469771d0b715f5c0bc6feaedb9
TobiVanHelsinki <TobiVanHelsinki@live.de>
2020-04-22 17:18:53 +0200

    created dynamic styles

--- Xamarin/ShadowRunHelperViewer/UI/Resources/SharedStyles.xaml.cs
+++ Xamarin/ShadowRunHelperViewer/UI/Resources/SharedStyles.xaml.cs
@@ -1,10 +1,8 @@
 ﻿//Author: Tobi van Helsinki
 
-using System;
 using ShadowRunHelper;
 using ShadowRunHelperViewer.UI.Resources;
 using TLIB;
-using Xamarin.Forms;
 
 namespace ShadowRunHelperViewer
 {
@@ -12,50 +10,20 @@ namespace ShadowRunHelperViewer
     {
         public SharedStyles()
         {
-            InitializeComponent();
+            Add("Spacing", SettingsModel.I.CurrentSpacingStrategy);
+            Add("SpacingS", (SettingsModel.I.CurrentSpacingStrategy - 1).LowerB(0));
+            Add("SpacingM", SettingsModel.I.CurrentSpacingStrategy);
+            Add("SpacingL", SettingsModel.I.CurrentSpacingStrategy + 2);
+            Add("CurrentBackgroundColor", StyleManager.BackgroundColor);
+            Add("CurrentForegroundColor", StyleManager.ForegroundColor);
+            Add("CurrentTextColor", StyleManager.TextColor);
+            Add("CurrentAccentColor", StyleManager.AccentColor);
+            Add("CurrentAccentHighColor", StyleManager.AccentHighColor);
+            Add("CurrentAccentLowColor", StyleManager.AccentLowColor);
+            Add("CurrentButtonColor", StyleManager.ButtonColor);
+            Add("CornerRadiusSize", 3); //TODO introduce option
 
-            foreach (var (name, type, basedon, setter) in new[] {
-                ("TemplateStack", typeof(StackLayout),"", new (BindableProperty, object)[] {
-                    (StackLayout.SpacingProperty, 0),
-                    (StackLayout.OrientationProperty, StackOrientation.Horizontal),
-                    (Layout.PaddingProperty, new Thickness(SettingsModel.I.CurrentSpacingStrategy)),
-                    (View.MarginProperty, new Thickness(0)),
-                    (View.VerticalOptionsProperty, LayoutOptions.Center ),
-                }),
-                ("TemplateGrid", typeof(Grid),"", new (BindableProperty, object)[] {
-                    (Grid.ColumnSpacingProperty, 0),
-                    (Grid.RowSpacingProperty, 0),
-                    (Layout.PaddingProperty, new Thickness(SettingsModel.I.CurrentSpacingStrategy)),
-                    (View.MarginProperty, new Thickness(0)),
-                    (View.VerticalOptionsProperty, LayoutOptions.Center ),
-                }),
-                ("ItemFrame", typeof(Frame),"", new (BindableProperty, object)[] {
-                    (Layout.PaddingProperty, new Thickness(SettingsModel.I.CurrentSpacingStrategy)),
-                    (View.MarginProperty, new Thickness(SettingsModel.I.CurrentSpacingStrategy)),
-                    (Frame.CornerRadiusProperty, 3f),
-                    (Frame.BorderColorProperty, Color.Black),
-                    (VisualElement.BackgroundColorProperty, StyleManager.BackgroundColor),
-                }),
-                })
-            {
-                try
-                {
-                    var style = new Style(type);
-                    if (!string.IsNullOrEmpty(basedon))
-                    {
-                        style.BaseResourceKey = basedon;
-                    }
-                    foreach (var (prop, value) in setter)
-                    {
-                        style.Setters.Add(new Setter() { Property = prop, Value = value });
-                    }
-                    Add(name, style);
-                }
-                catch (Exception ex)
-                {
-                    Log.Write("Could not create resource", ex, logType: LogType.Error);
-                }
-            }
+            InitializeComponent();
         }
     }
 }
--- Xamarin/ShadowRunHelperViewer/UI/Resources/StyleManager.cs
+++ Xamarin/ShadowRunHelperViewer/UI/Resources/StyleManager.cs
@@ -12,15 +12,35 @@ namespace ShadowRunHelperViewer.UI.Resources
             Constants.StyleBrigth => Color.Beige,
             Constants.StyleDark => Color.DarkSlateGray,
             Constants.StyleScaryGreen => Color.DarkGreen,
-            _ => Color.White,
+            _ => Color.Transparent,
         };
 
         internal static Color ForegroundColor => SettingsModel.I.CurrentStyleName switch
         {
             Constants.StyleBrigth => Color.DarkSlateGray,
-            Constants.StyleDark => Color.Beige,
+            Constants.StyleDark => Color.FromRgb(255, 248, 231), //Cosmic Latte
             Constants.StyleScaryGreen => Color.GreenYellow,
-            _ => Color.White,
+            _ => Color.Transparent,
         };
+
+        internal static Color TextColor => SettingsModel.I.CurrentStyleName switch
+        {
+            Constants.StyleBrigth => Color.Black,
+            Constants.StyleDark => Color.FromRgb(255, 248, 231), //Cosmic Latte
+            Constants.StyleScaryGreen => Color.Yellow,
+            _ => Color.Transparent,
+        };
+
+        internal static Color ButtonColor => SettingsModel.I.CurrentStyleName switch
+        {
+            Constants.StyleBrigth => Color.Gray,
+            Constants.StyleDark => Color.LightGray,
+            Constants.StyleScaryGreen => Color.PaleGreen,
+            _ => Color.Transparent,
+        };
+
+        internal static Color AccentColor => Color.Accent;
+        internal static Color AccentHighColor => Color.Accent.AddLuminosity(.2);
+        internal static Color AccentLowColor => Color.Accent.AddLuminosity(-.2);
     }
 }
